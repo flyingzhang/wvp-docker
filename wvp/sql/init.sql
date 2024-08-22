e4d28587fc0896b0c16739fcb798e622effec2a2
zhangzp <zhangzp@qq.com>
2024-08-22 14:34:43 +0800

    sync db script with wvp-pro 2.7.3

--- wvp/sql/init.sql
+++ wvp/sql/init.sql
@@ -1,9 +1,3 @@
-create database if not exists wvp default character set utf8mb4 collate utf8mb4_general_ci;
-use wvp;
-
-/* 以上2句别删，修改脚本从下面开始粘贴 */
-
-/*建表*/
 /*建表*/
 create table wvp_device (
                             id serial primary key ,
@@ -343,5 +337,6 @@ create table wvp_user_api_key (
 /*初始数据*/
 INSERT INTO wvp_user VALUES (1, 'admin','21232f297a57a5a743894a0e4a801fc3',1,'2021-04-13 14:14:57','2021-04-13 14:14:57','3e80d1762a324d5b0ff636e0bd16f1e3');
 INSERT INTO wvp_user_role VALUES (1, 'admin','0','2021-04-13 14:14:57','2021-04-13 14:14:57');
+
 
 

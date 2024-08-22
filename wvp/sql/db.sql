f33fa4008f6a872453342a2ffbb170ca8230a1a7
zhangzp <zhangzp@qq.com>
2024-08-22 14:24:52 +0800

    checkpoint: updates for latest wvp

--- wvp/sql/db.sql
+++ wvp/sql/db.sql
@@ -2,6 +2,7 @@ create database if not exists wvp default character set utf8mb4 collate utf8mb4_
 use wvp;
 
 /*建表*/
+/*建表*/
 create table wvp_device (
                             id serial primary key ,
                             device_id character varying(50) not null ,
@@ -34,7 +35,6 @@ create table wvp_device (
                             password character varying(255),
                             as_message_channel bool default false,
                             keepalive_interval_time integer,
-                            switch_primary_sub_stream bool default false,
                             broadcast_push_after_ack bool default false,
                             constraint uk_device_device unique (device_id)
 );
@@ -95,6 +95,7 @@ create table wvp_device_channel (
                                     latitude_wgs84 double precision,
                                     business_group_id character varying(50),
                                     gps_time character varying(50),
+                                    stream_identification character varying(50),
                                     constraint uk_wvp_device_channel_unique_device_channel unique (device_id, channel_id)
 );
 
@@ -157,8 +158,13 @@ create table wvp_media_server (
                                   rtp_proxy_port integer,
                                   rtsp_port integer,
                                   rtsp_ssl_port integer,
+                                  flv_port integer,
+                                  flv_ssl_port integer,
+                                  ws_flv_port integer,
+                                  ws_flv_ssl_port integer,
                                   auto_config bool default false,
                                   secret character varying(50),
+                                  type character varying(50) default 'zlm',
                                   rtp_enable bool default false,
                                   rtp_port_range character varying(50),
                                   send_rtp_port_range character varying(50),
@@ -167,6 +173,9 @@ create table wvp_media_server (
                                   create_time character varying(50),
                                   update_time character varying(50),
                                   hook_alive_interval integer,
+                                  record_path character varying(255),
+                                  record_day integer default 7,
+                                  transcode_suffix character varying(255),
                                   constraint uk_media_server_unique_ip_http_port unique (ip, http_port)
 );
 
@@ -198,6 +207,7 @@ create table wvp_platform (
                               update_time character varying(50),
                               as_message_channel bool default false,
                               auto_push_channel bool default false,
+                              send_stream_ip character varying(50),
                               constraint uk_platform_unique_server_gb_id unique (server_gb_id)
 );
 
@@ -262,12 +272,29 @@ create table wvp_stream_push (
                                  create_time character varying(50),
                                  alive_second integer,
                                  media_server_id character varying(50),
+                                 server_id character varying(50),
                                  push_time character varying(50),
                                  status bool default false,
                                  update_time character varying(50),
                                  push_ing bool default false,
                                  self bool default false,
                                  constraint uk_stream_push_app_stream unique (app, stream)
+);
+create table wvp_cloud_record (
+                                  id serial primary key,
+                                  app character varying(255),
+                                  stream character varying(255),
+                                  call_id character varying(255),
+                                  start_time bigint,
+                                  end_time bigint,
+                                  media_server_id character varying(50),
+                                  file_name character varying(255),
+                                  folder character varying(255),
+                                  file_path character varying(255),
+                                  collect bool default false,
+                                  file_size bigint,
+                                  time_len bigint,
+                                  constraint uk_stream_push_app_stream_path unique (app, stream, file_path)
 );
 
 create table wvp_user (
@@ -298,10 +325,21 @@ create table wvp_resources_tree (
                                     path character varying(255)
 );
 
+create table wvp_user_api_key (
+                                  id serial primary key ,
+                                  user_id bigint,
+                                  app character varying(255) ,
+                                  api_key text,
+                                  expired_at bigint,
+                                  remark character varying(255),
+                                  enable bool default true,
+                                  create_time character varying(50),
+                                  update_time character varying(50)
+);
+
 
 /*初始数据*/
 INSERT INTO wvp_user VALUES (1, 'admin','21232f297a57a5a743894a0e4a801fc3',1,'2021-04-13 14:14:57','2021-04-13 14:14:57','3e80d1762a324d5b0ff636e0bd16f1e3');
 INSERT INTO wvp_user_role VALUES (1, 'admin','0','2021-04-13 14:14:57','2021-04-13 14:14:57');
-
 
 
